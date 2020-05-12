edd674df44d842511ebbf8b85da79ac89dffb263
GeorgCantor <cupsman1986@gmail.com>
2020-05-12 20:34:27 +0300

    removed unused import

--- app/src/main/kotlin/io/github/erikjhordanrey/arch_components_paging_library/view/MoviesActivity.kt
+++ app/src/main/kotlin/io/github/erikjhordanrey/arch_components_paging_library/view/MoviesActivity.kt
@@ -2,7 +2,6 @@ package io.github.erikjhordanrey.arch_components_paging_library.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import io.github.erikjhordanrey.arch_components_paging_library.R
